a699a819b11c8c42ba065815ceeec652d616c242
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2021-02-05 09:56:32 +0530

    Shubham

--- 100.c
+++ 100.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 int main()
 {
+    char operator;
+    double n1, n2;
+    printf("enter an operator(+, -, *, /)" );
+    scanf("%c", &operator);
+    printf("enter two number: ");
+    scanf("%lf %lf", &n1, &n2);
+
+    switch (operator)
+    {
+    case '+':
+
+    printf("%.1lf + %.1lf = %.1lf", n1, n2, n1 + n2);
+    break;
+     case '-':
+
+    printf("%.1lf - %.1lf = %.1lf", n1, n2, n1 - n2);
+    break;
+     case '*':
+
+    printf("%.1lf * %.1lf = %.1lf", n1, n2, n1 * n2);
+    break;
+     case '/':
+
+    printf("%.1lf / %.1lf = %.1lf", n1, n2, n1 / n2);
+    break;
+    default:
+    printf("Error! operator is not correct");
+    }
+
+
     
     return 0;
 }
--- 98.c
+++ 98.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 int main()
 {
-    
+    int i;
+    double number , sum = 0.0;
+
+    for(i = 1; i <= 10; ++i)
+    {
+    printf("enter a n%d: ", i);
+    scanf("%lf", &number);
+    if(number < 0.0)
+{
+    break;
+
+}
+sum += number;
+    }
+printf("sum = %.2lf", sum);
+
     return 0;
 }
