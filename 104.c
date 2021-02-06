c25a9f565dfd6af84541c1ee8f01da298773b1a1
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2021-02-06 21:14:26 +0530

    Shubham

--- 104.c
+++ 104.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+int getInteger();
+
 int main()
 {
+    int i, n, flag = 0;
+    n = getInteger();
+    for (i=2; i<=n/2; ++i)
+    {
+        if(n%i==0)
+        {
+            flag = 1;
+            break;
+
+        }
+    }
+    if (flag == 1)
+    printf("%d is not a prime number", n);
+    else
+    printf("%d is  a prime number", n);
+    
     
     return 0;
+}
+int getInteger()
+{
+    int n;
+    printf("enter a positive number: ");
+    scanf("%d", &n);
+    return n;
+
 }
